b9dfb9a9d8af414301e2217bec4fac95bbcfac59
Watunder <vinschen66@gmail.com>
2025-06-18 03:20:20 +0800

    Update the build configurations

--- src/js/license.js
+++ src/js/license.js
@@ -1,6 +1,6 @@
 /*!
- *  Effekseer for WebGL v1.70
- *  https://github.com/effekseer/EffekseerForWebGL
+ *  Effekseer for WebGL
+ *  https://github.com/Watunder/EffekseerForWebGL
  *
  *  This software is licensed under the MIT License.
  *  http://www.opensource.org/licenses/mit-license
